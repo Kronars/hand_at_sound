6b266e755a4c85366b423d1a0021c3bd15f0f5f0
IronNanny <79563043+IronNanny@users.noreply.github.com>
2023-06-11 16:39:41 +0300

    Убрал лишние функции из исходного примера

--- examples/getGyroData/testGyro.py
+++ examples/getGyroData/testGyro.py
@@ -1,43 +1,25 @@
 import smbus
 import math
- 
+
 # параметры инициализации гироскопа
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
- 
-def read_byte(reg):
-    return bus.read_byte_data(address, reg)
- 
-def read_word(reg):
-    h = bus.read_byte_data(address, reg)
-    l = bus.read_byte_data(address, reg+1)
-    value = (h << 8) + l
-    return value
- 
+
+# функция получения данных в ответ на команду reg
 def read_word_2c(reg):
     val = read_word(reg)
     if (val >= 0x8000):
         return -((65535 - val) + 1)
     else:
         return val
- 
-def dist(a,b):
-    return math.sqrt((a*a)+(b*b))
- 
-def get_y_rotation(x,y,z):
-    radians = math.atan2(x, dist(y,z))
-    return -math.degrees(radians)
- 
-def get_x_rotation(x,y,z):
-    radians = math.atan2(y, dist(x,z))
-    return math.degrees(radians)
- 
+
 bus = smbus.SMBus(1)
 address = 0x68       # адрес гиро на шине i2c
- 
-# инициализация
+
+# инициализация датчика
 bus.write_byte_data(address, power_mgmt_1, 0)
 
+# функция получения всех данных
 def get_data():
     gyro_xout = read_word_2c(0x43)
     gyro_yout = read_word_2c(0x45)
